8ba1540533a457e2002ef4eb116d97ba826b7f6a
Nick Tidswell <nicktids@gmail.com>
2019-07-11 19:04:03 +0800

    db and models in to notebook create_all

--- app/models.py
+++ app/models.py
@@ -1,12 +1,18 @@
 from app import db
+from sqlalchemy import BINARY, Column, Date, Float, Integer, MetaData,Unicode,String,Numeric,DECIMAL,TypeDecorator,BigInteger,ForeignKey
 
 class Station(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    station = db.Column(db.String(64), index=True, unique=True)
-    country = db.Column(db.String(64), index=True)
+    id = Column(Integer, primary_key=True)
+    station = Column(String(64), index=True, unique=True)
+    country = Column(String(64), index=True)
 
     def __repr__(self):
         return '<station {}>'.format(self.station)
 
 class Broadcaster(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100),unique=True)
+    
+
+
+    
